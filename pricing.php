6024c44651d96b036b3d5f9ff73ad1d12d4e5e99
Samuelzinh0 <samuelzinhomoreira@outlook.com>
2020-05-06 15:38:00 -0600

    adding a pricing page

--- pricing.php
+++ pricing.php
@@ -10,7 +10,7 @@
 
 <body>
   <div class="jumbotron text-center">
-    <h1>Welcome to the Pricing Page!</h1>
+    <h1>Pricing</h1>
   </div>
     <div class="text-center">
     <nav class="navbar navbar-inverse">
@@ -30,7 +30,7 @@
           we are able to offer we don't have an explicit flat rate. You as our prized customer needs to call our Owner to set up a time to have him come and do the preparation 
           work and give you an accurate estimation for the job requested. His contact information is (208) 390-3783 </h2>
     <h3> These are the different types of molds that we offer: </h3>
-    <img src="/images/curbs_group1.jpg" width="50%" height="50%" alt="Molds Example">
+    <img src="/images/curbs_group1.jpg" width="50%" height="25%" alt="Molds Example">
     </div>
 
     <?php
